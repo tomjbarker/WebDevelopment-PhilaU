f194dd6af10fa508eb6473e453a3944b1a1c9c81
Barker <tbarke000@HQSML-126488.(none)>
2015-09-17 10:19:02 -0400

    adding week 4

---
+++ week4.js
@@ -0,0 +1,6 @@
+var my_list = [20, 1003, 394, 2];
+
+for (var x = 0; x < my_list.length; x++){
+alert("length of array: " + my_list.length + " value of x: " + x + " value in array: " + my_list[x]); // using x as the index number
+}
+
---
+++ week_4_class_notes.md
@@ -0,0 +1,5 @@
+#Week 4 Class Notes
+
+##JavaScript Primer
+https://www.dropbox.com/s/juttbg5w3b5jo90/9781430237891_Ch08.pdf?dl=0
+
